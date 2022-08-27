507696684e8eb864b7d348ce673646bd39528e4b
Rim <92591903+httpsrim@users.noreply.github.com>
2022-08-27 18:23:48 +0200

    Add files via upload

---
+++ Operaciones/Operaciones.java
@@ -0,0 +1,27 @@
+public class Operaciones{
+    public static void main(String[] args){
+        int primerNumero = 4;
+        int segundoNumero = 3;
+        int resultado;
+        //suma
+            resultado = primerNumero + segundoNumero;
+            System.out.println(resultado);
+        //restaç
+            resultado = primerNumero - segundoNumero;
+            System.out.println(resultado);
+        //multiplicar 
+            resultado = primerNumero * segundoNumero;
+            System.out.println(resultado);
+        //dividir
+            resultado = primerNumero / segundoNumero;
+            System.out.println(resultado);
+        //resto
+            resultado = primerNumero % segundoNumero;
+            System.out.println(resultado);
+            System.out.println((2*4+3) == (3/2+4));
+            System.out.println((2*4+3) != (3/2+4));
+            System.out.println((2*4+3) > (3/2+4));
+            System.out.println((2*4+3) >= (3/2+4));
+            System.out.println((2*4+3) <= (3/2+4));
+        }    
+}
