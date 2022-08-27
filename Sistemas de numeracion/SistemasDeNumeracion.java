8184b5072049ca8f36b12799646553c54cb112f9
Rim <92591903+httpsrim@users.noreply.github.com>
2022-08-27 17:48:41 +0200

    Add files via upload

---
+++ Sistemas de numeracion/SistemasDeNumeracion.java
@@ -0,0 +1,12 @@
+class numeros{
+    public static void main(String[] args){
+      int bin = 0b1011;
+      int hexa = 0xff;
+      int octal = 023;
+      int decimal = 231;
+      System.out.println(bin);
+      System.out.println(hexa);
+      System.out.println(octal);
+      System.out.println(decimal);
+    }
+}
