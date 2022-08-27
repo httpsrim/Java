f7c946f080ad34fb0402a8cadfab05c30688a238
Rim <92591903+httpsrim@users.noreply.github.com>
2022-08-27 16:24:15 +0200

    Update HelloWorld.java

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,8 +1,14 @@
-class helloworld{
     /**
      * {@summary}Primer programa en Java, es un Hello World
      */
+class helloworld{
     public static void main(String[] args) {
         System.out.print("Hello World!");
+        System.out.print(" ");
+        System.out.print("Hello");
+        System.out.print(" ");
+        System.out.print("World");
+        System.out.println("Hello World");
+        System.out.print("Hello"+" "+"world");
     }
 }
